1bf80184e50db8746b97c9db643f52c67aff659a
Amin Jamali <53099680+BIGboss248@users.noreply.github.com>
2026-01-02 19:09:05 +0330

    Updated event card to show more properties

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -10,13 +10,28 @@ interface Props {
     time: string;
 }
 
-const EventCard = ({ title, image }: Props) => {
+const EventCard = ({ title, image, slug, location, date, time }: Props) => {
     return (
-        <Link href={'/events'} id="event-card">
+        <Link href={'/events/${slug}'} id="event-card">
             <Image src={image} alt={title} width={410} height={300} className="poster"></Image>
+            <div className="flex flex-row gap-2">
+
+                <Image src={'/icons/pin.svg'} alt="Location" width={14} height={14}></Image>
+                <p>{location}</p>
+            </div>
             <p className="title">
                 {title}
             </p>
+            <div className="datetime">
+                <div>
+                    <Image src={'/icons/calendar.svg'} alt="date" width={14} height={14}></Image>
+                    <p>{date}</p>
+                </div>
+                <div>
+                    <Image src={'/icons/clock.svg'} alt="time" width={14} height={14}></Image>
+                    <p>{time}</p>
+                </div>
+            </div>
         </Link>
     )
 }
