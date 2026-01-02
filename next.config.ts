39515bffc16a7bcacecf5d20838c6a2cf508362f
Amin Jamali <53099680+BIGboss248@users.noreply.github.com>
2026-01-02 19:47:13 +0330

    Added  posthog

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  experimental:{
+  experimental: {
     turbopackFileSystemCacheForDev: true,
-  }
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+    ];
+  },
+
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
