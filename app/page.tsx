4dd6ec1f5d8b011512f6d67da9bba56b4ddefc10
Amin Jamali <53099680+BIGboss248@users.noreply.github.com>
2026-01-02 18:56:47 +0330

    Updated event card and added dummy data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
+import { events } from "@/lib/constants"
 
 const Home = () => {
   return (
@@ -11,6 +13,21 @@ const Home = () => {
         Hacakthons, meetups, and confrences all in one place
       </p>
       <ExploreBtn />
+      <div className="mt-20 space-y-7">
+        <h3>Featured events</h3>
+
+        <ul className="events">
+          {
+            events.map((e) =>
+              <li key={e.title}>
+
+                <EventCard {...e} />
+              </li>
+            )
+          }
+        </ul>
+
+      </div>
     </section>
   )
 }
